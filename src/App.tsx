fb06db8c0441e9832040ede26765f7cd79feb537
Troli <brawlixo123@gmail.com>
2023-10-13 17:05:54 -0300

    first commit and project done!

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import Header from './components/Cabecalho'
-import Hero from './components/Hero'
+import Cabecalho from './components/Header'
+import HeroPage from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
-
-import './global.css'
+import EstiloGlobal, { Container } from './style'
 
 function App() {
   return (
     <>
-      <Header />
-      <Hero />
-      <div className="container">
+      <EstiloGlobal />
+      <Cabecalho />
+      <HeroPage />
+      <Container>
         <ListaVagas />
-      </div>
+      </Container>
     </>
   )
 }
--- src/components/Vaga/index.tsx
+++ src/components/Vaga/index.tsx
@@ -1,4 +1,4 @@
-import styles from './Vaga.module.css'
+import Vaga, { VagaTitulo, VagaRequisitos } from './Vaga'
 
 type Props = {
   titulo: string
@@ -10,9 +10,9 @@ type Props = {
   requisitos: string[]
 }
 
-const Vaga = (props: Props) => (
-  <li className={styles.vaga}>
-    <h3 className={styles.vagaTitulo}>{props.titulo}</h3>
+const Vagas = (props: Props) => (
+  <Vaga>
+    <VagaTitulo />
     <ul>
       <li>Localizacao: {props.localizacao}</li>
       <li>Senioridade: {props.nivel}</li>
@@ -22,10 +22,8 @@ const Vaga = (props: Props) => (
       </li>
       <li>Requisitos: {props.requisitos.join(', ')}</li>
     </ul>
-    <a className={styles.vagaLink} href="#">
-      Ver detalhes e candidatar-se
-    </a>
-  </li>
+    <VagaRequisitos>Ver detalhes e candidatar-se</VagaRequisitos>
+  </Vaga>
 )
 
-export default Vaga
+export default Vagas
